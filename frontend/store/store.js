f39797e909fac3e2b04365cae84948af2084f21c
bertmeeuws <bert.meeuws@student.howest.be>
2021-06-11 20:47:10 +0200

    Inserting some global state #7

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,29 @@
 import { createStore, action, persist } from "easy-peasy";
 
-const model = {
+export const model = {
   todos: {
     items: ["Create store", "Wrap application", "Use store"],
-    add: action((state, payload) => {
-      state.items.push(payload);
+  },
+  addToDo: action((state, payload) => {
+    state.todos.items.push(payload);
+  }),
+  route: {
+    id: 0,
+    changeRoute: action((state, payload) => {
+      state.route.id = payload;
+    }),
+  },
+  user: {
+    user: null,
+    changeUser: action((state, payload) => {
+      state.user.user = payload;
+    }),
+  },
+  profile: {
+    profile_picture: null,
+    //Base64 image
+    changeProfilePicture: action((state, payload) => {
+      state.profile.profile_picture = payload;
     }),
   },
 };
