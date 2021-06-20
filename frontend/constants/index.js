acd29daa835b722a385e87e9ef2323ddc62ea159
bertmeeuws <bert.meeuws@student.howest.be>
2021-06-20 18:50:20 +0200

    Pushing some code for prod build

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -1,5 +1,5 @@
 //export const API_URL = "http://localhost:1337";
 export const API_URL = "https://integration4strapi.herokuapp.com";
-export const WEBSITE_URL = "http://localhost:3000";
+export const WEBSITE_URL = "https://mts-4-integration4-team5.vercel.app/";
 
 export const JWT_KEY = "he4564erg986vvsd6g4rge91dfs365v";
