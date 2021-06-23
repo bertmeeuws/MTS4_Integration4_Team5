6b2301e2b549f6821bffc72d8c843181b38561dc
bertmeeuws <bert.meeuws@student.howest.be>
2021-06-23 10:29:35 +0200

    Quick fix state

--- frontend/components/Layout/Nav.jsx
+++ frontend/components/Layout/Nav.jsx
@@ -7,20 +7,18 @@ export default function Nav({ children }) {
   return (
     <>
       <nav className="nav">
-        
         <div className="nav-container">
+          <Image src="/assets/img/logo.svg" width={89} height={46.05} />
 
-          <Image
-            src="/assets/img/logo.svg"
-            width={89} height={46.05}
-          />
-
-          <button onClick={(e) => signIn()} href="login" className="button__header title__s-bold">
+          <button
+            style={{ cursor: "pointer" }}
+            onClick={(e) => signIn()}
+            href="login"
+            className="button__header title__s-bold"
+          >
             Inloggen als leerkracht
           </button>
-
         </div>
-      
       </nav>
       {children}
     </>
