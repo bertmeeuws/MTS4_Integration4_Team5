7504311059210b38fa803bcb0e655bed5fc911b6
bertmeeuws <bert.meeuws@student.howest.be>
2021-06-23 19:47:13 +0200

    Setting up env. variables for deploy #39

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -1,6 +1,10 @@
-export const API_URL = "http://localhost:1337";
+//export const API_URL = "http://localhost:1337";
 //export const API_URL = "https://integration4strapi.herokuapp.com";
 //export const WEBSITE_URL = "https://mts-4-integration4-team5.vercel.app";
-export const WEBSITE_URL = "http://localhost:3000";
+//export const WEBSITE_URL = "http://localhost:3000";
+export const WEBSITE_URL =
+  process.env.NEXT_PUBLIC_WEBSITE_URL || "http://localhost:3000";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 
 export const JWT_KEY = "he4564erg986vvsd6g4rge91dfs365v";
